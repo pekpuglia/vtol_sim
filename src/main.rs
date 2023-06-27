d3fd886cb7dddf04234ebeb64e663795fbe92954
pekpuglia <pepuglia@gmail.com>
2023-06-27 16:19:01 -0300

    basic egaku example

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use egaku2d::glutin::{self, event::VirtualKeyCode, event_loop::ControlFlow};
+
+fn main() {
+    let ev_loop = glutin::event_loop::EventLoop::new();
+    let mut glsys = egaku2d::WindowedSystem::new(
+        [600, 480], 
+        &ev_loop, 
+        "test window");
+
+
+
+    let mut timer = egaku2d::RefreshTimer::new(16);
+
+    ev_loop.run(move |event, _, control_flow| match event {
+        glutin::event::Event::WindowEvent {event: wev, .. } => {
+            match wev {
+                glutin::event::WindowEvent::KeyboardInput { input, ..} => {
+                    if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                },
+                _ => {}
+            }
+        },
+        glutin::event::Event::MainEventsCleared => {
+            if timer.is_ready() {
+                let canvas = glsys.canvas_mut();
+                canvas.clear_color([0.0,0.0,0.0]);
+                canvas.rects()
+                .add([100.0, 500.0, 50.0, 430.0])
+                .send_and_uniforms(canvas)
+                .with_color([1.0,1.0,1.0,1.0]).draw();
+                glsys.swap_buffers()
+            }
+        }
+        _ => {}
+    });
+}
