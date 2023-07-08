856e054d3e33b9307f992f8fcef0466f828f3d88
pekpuglia <pepuglia@gmail.com>
2023-07-08 15:59:52 -0300

    mostly works

--- src/ball.rs
+++ src/ball.rs
@@ -36,8 +36,11 @@ impl System<Vector4<f64>> for Ball {
         dy.w = -y.w*self.damp;
     }
 
-    // fn solout(&mut self, _x: f64, _y: &Vector4<f32>, _dy: &Vector4<f32>) -> bool {
-        
+    // fn solout(&mut self, _x: f64, _y: &Vector4<f64>, _dy: &Vector4<f64>) -> bool {
+    //     self.wall_point_and_normals
+    //         .iter()
+    //         .map(|el| (el.0, el.1.1.dot(self.pos - el.1.0)))
+    //         .filter(|wall|)
     // }
 }
 
@@ -78,7 +81,9 @@ impl Ball {
     fn dynamics(&mut self, dt: f32) {
         match self.is_held {
             true => {
-                self.pos = self.current_mouse_pos;
+                self.pos = self.current_mouse_pos
+                    .zip_map(&Vector2::new(self.r, self.r), |v1, v2| v1.max(v2))
+                    .zip_map(&Vector2::new(WID-self.r, HEI-self.r), |v1, v2| v1.min(v2));
                 self.vel = (self.current_mouse_pos - self.last_mouse_pos) / dt;
             },
             false => {
@@ -94,10 +99,14 @@ impl Ball {
                         free_movement_time.into(), 
                         free_movement_time as f64 / 5.0);
 
-                    let _stats = integrator.integrate();
+                    let stats = integrator.integrate();
+                    
 
                     let new_state_vec = integrator.y_out().last().expect("should have integrated at least 1 step").to_owned();
-                    self.pos = Vector2::new(new_state_vec.x as f32, new_state_vec.y as f32);
+                    self.pos = Vector2::new(new_state_vec.x as f32, new_state_vec.y as f32)
+                        .zip_map(&Vector2::new(self.r, self.r), |v1, v2| v1.max(v2))
+                        .zip_map(&Vector2::new(WID-self.r, HEI-self.r), |v1, v2| v1.min(v2));
+                    
                     self.vel = Vector2::new(new_state_vec.z as f32, new_state_vec.w as f32);
 
                     let free_movement_time: f32 = integrator.x_out().last().expect("should have integrated at elast 1 step").to_owned() as f32;
@@ -111,11 +120,7 @@ impl Ball {
                 }
             },
         }
-        self.pos = self.pos
-            .zip_map(&Vector2::new(self.r, self.r), |v1, v2| v1.max(v2))
-            .zip_map(&Vector2::new(WID-self.r, HEI-self.r), |v1, v2| v1.min(v2))
-            // .zip([0.0   + self.r, 0.0   + self.r].into(), f32::max)
-            // .zip([WID - self.r, HEI - self.r].into(), f32::min);
+        
     }
 }
 
