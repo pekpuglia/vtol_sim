eddf574c3b3c2f958610a42b2afb30abc5a5e365
pekpuglia <pepuglia@gmail.com>
2023-07-11 18:30:41 -0300

    bicopter simple control

--- src/bicopter.rs
+++ src/bicopter.rs
@@ -1,4 +1,6 @@
-use ode_solvers::{Vector2, Vector6};
+use egaku2d::glutin::event::{Event, WindowEvent, MouseButton, ElementState};
+use nalgebra::{Rotation2, Vector2};
+use ode_solvers::Vector6;
 use ode_solvers::{Rk4, System};
 
 use crate::graphical_utils::*;
@@ -17,15 +19,15 @@ struct Bicopter {
     mass: f32,
     gravity: f32,
     prop_dist: f32,
-    max_thrust: f32,
     l_thrust: f32,
-    r_thrust:f32
+    r_thrust:f32,
+    max_thrust: f32,
+    paused: bool
 }
 
 impl System<Vector6<f64>> for Bicopter {
     fn system(&self, x: f64, y: &Vector6<f64>, dy: &mut Vector6<f64>) {
-        let propeller_direction = Vector2::new(self.angle_rad.sin(), -self.angle_rad.cos());
-        let thrust = (self.l_thrust + self.r_thrust) * propeller_direction;
+        let thrust = (self.l_thrust + self.r_thrust) * self.propeller_direction();
         dy.x = y.w;
         dy.y = y.a;
         dy.z = y.b;
@@ -36,8 +38,12 @@ impl System<Vector6<f64>> for Bicopter {
 }
 
 impl Bicopter {
+
+    fn propeller_direction(&self) -> Vector2<f32> {
+        Vector2::new(self.angle_rad.sin(), -self.angle_rad.cos())
+    }
+
     fn update(&mut self, dt: f64) {
-        dbg!(self.state_vector());
         let mut stepper = Rk4::new(*self, 
             0.0, 
             self.state_vector(), 
@@ -69,18 +75,62 @@ impl Bicopter {
 
 impl Component for Bicopter {
     fn draw(&mut self, canvas: &mut egaku2d::SimpleCanvas, dt: f32) {
-        self.update(dt as f64);
-        let left_right = Vector2::new(self.angle_rad.cos(), self.angle_rad.sin());
+        if !self.paused {
+            self.update(dt as f64);
+        }
+
+        let prop_dir = self.propeller_direction();
+        
+        let left_right = Rotation2::new(std::f32::consts::FRAC_PI_2) * prop_dir;
+
+        let left = self.pos - self.prop_dist/2.0 * left_right;
+
+        let right =  self.pos + self.prop_dist/2.0 * left_right;
+
+        //corpo
         canvas
             .lines(5.0)
-            .add((self.pos - self.prop_dist/2.0 * left_right).into(), (self.pos + self.prop_dist/2.0 * left_right).into())
+            .add(left.into(),right.into())
             .send_and_uniforms(canvas)
             .with_color([1.0, 1.0, 1.0, 1.0])
             .draw();
+
+        //empuxos
+        canvas
+            .arrows(2.0)
+            .add(left.into(), (left + 70.0 * self.l_thrust/self.max_thrust * prop_dir).into())
+            .send_and_uniforms(canvas)
+            .with_color([1.0, 0.0, 0.0, 1.0])
+            .draw();
+
+        canvas
+            .arrows(2.0)
+            .add(right.into(), (right + 70.0 * self.r_thrust/self.max_thrust * prop_dir).into())
+            .send_and_uniforms(canvas)
+            .with_color([0.0, 0.0, 1.0, 1.0])
+            .draw();
+
     }
 
     fn receive_event(&mut self, ev: &egaku2d::glutin::event::Event<'_, ()>) {
-        
+        if let Event::WindowEvent {event: WindowEvent::CursorMoved { device_id: _, position: p, .. }, window_id: _} = ev {
+            let total_thrust = 2.0 * self.max_thrust * (1.0 - (p.y as f32)/HEI);
+
+            let moment = 1e-1 * self.max_thrust * self.prop_dist * ((p.x as f32)/WID - 1.0/2.0);
+
+            self.l_thrust = total_thrust / 2.0 + moment / (2.0 * self.prop_dist);
+            self.r_thrust = total_thrust / 2.0 - moment / (2.0 * self.prop_dist);
+            dbg!(total_thrust, moment);
+        }
+
+        if let Event::WindowEvent { window_id, 
+            event: WindowEvent::MouseInput { 
+                device_id, state, button, modifiers } } = ev {
+            match (state, button) {
+                (ElementState::Pressed, MouseButton::Right) => {self.paused = !self.paused},
+                _ => {}
+            }
+        }
     }
 }
 
@@ -96,16 +146,17 @@ pub fn bicopter_main() {
         vec![
             Box::new(Bicopter{
                 pos:[WID/2.0,HEI/2.0].into(),
-                angle_rad:std::f32::consts::FRAC_PI_4,
-                mass:1.1,
-                inertia:1e-2,
+                angle_rad: 0.0,
+                mass:1.0,
+                inertia:100.0,
                 gravity:100.0,
                 prop_dist:40.0,
-                max_thrust:0.7,
-                l_thrust:0.5+1e-3,
-                r_thrust:0.5-1e-3, 
+                max_thrust:100.0,
+                l_thrust:0.0,
+                r_thrust:0.0, 
                 vel: [0.0,0.0].into(), 
-                ang_vel: 1.0 })
+                ang_vel: 0.0,
+                paused: true })
         ]);
 
     ev_loop.run(move |event, _, control_flow| main_loop(event, control_flow, &mut drawer));
