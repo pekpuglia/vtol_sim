8e83b689fba7de420215ca211bde2edfd9f65ae8
pekpuglia <pepuglia@gmail.com>
2023-07-12 18:29:00 -0300

    works

--- src/bicopter.rs
+++ src/bicopter.rs
@@ -5,24 +5,58 @@ const WID: f32 = 600.0;
 
 const HEI: f32 = 480.0;
 
-mod bicopter_physics;
+mod bicopter_plant;
+
+struct BicopterForceMomentController {
+    force_gain: f32,
+    moment_gain: f32,
+    force: f32,
+    moment: f32
+}
+
+impl BicopterForceMomentController {
+    fn new(force_gain: f32, moment_gain: f32) -> BicopterForceMomentController {
+        BicopterForceMomentController { 
+            force_gain, 
+            moment_gain, 
+            force: 0.0, 
+            moment: 0.0 }
+    }
+
+    fn set_force_moment(&mut self, ev: &egaku2d::glutin::event::Event<'_, ()>) {
+        if let Event::WindowEvent {event: WindowEvent::CursorMoved { device_id: _, position: p, .. }, window_id: _} = ev {
+            
+            self.force = self.force_gain * (1.0 - (p.y as f32)/HEI);
+
+            self.moment = self.moment_gain * ((p.x as f32)/WID - 1.0/2.0);
+
+        }
+    }
+
+    //transformar em HAL
+    fn control_bicopter(&self, bicopter: &mut bicopter_plant::BicopterPlant) {
+        bicopter.l_thrust = self.force / 2.0 + self.moment / (2.0 * bicopter.prop_dist());
+        bicopter.r_thrust = self.force / 2.0 - self.moment / (2.0 * bicopter.prop_dist());
+    }
+}
 
 struct Bicopter {
-    bicopter_model: bicopter_physics::BicopterModel,
-    max_thrust: f32
+    plant: bicopter_plant::BicopterPlant,
+    controller: BicopterForceMomentController
 }
 
 impl Component for Bicopter {
     fn draw(&mut self, canvas: &mut egaku2d::SimpleCanvas, dt: f32, paused: bool) {
         if !paused {
-            self.bicopter_model.update(dt as f64);
+            self.controller.control_bicopter(&mut self.plant);
+            self.plant.update(dt as f64);
         }
 
-        let prop_dir = self.bicopter_model.propeller_direction();
+        let prop_dir = self.plant.propeller_direction();
 
-       let (left, right) = self.bicopter_model.left_right_positions();
+       let (left, right) = self.plant.left_right_positions();
 
-        let (l_thrust, r_thrust) = self.bicopter_model.thrusts();
+        let (l_thrust, r_thrust) = self.plant.thrusts();
 
         //corpo
         canvas
@@ -35,14 +69,14 @@ impl Component for Bicopter {
         //empuxos
         canvas
             .arrows(2.0)
-            .add(left.into(), (left + 70.0 * l_thrust/self.max_thrust * prop_dir).into())
+            .add(left.into(), (left + 0.7 * l_thrust * prop_dir).into())
             .send_and_uniforms(canvas)
             .with_color([1.0, 0.0, 0.0, 1.0])
             .draw();
 
         canvas
             .arrows(2.0)
-            .add(right.into(), (right + 70.0 * r_thrust/self.max_thrust * prop_dir).into())
+            .add(right.into(), (right + 0.7 * r_thrust * prop_dir).into())
             .send_and_uniforms(canvas)
             .with_color([0.0, 0.0, 1.0, 1.0])
             .draw();
@@ -50,14 +84,7 @@ impl Component for Bicopter {
     }
 
     fn receive_event(&mut self, ev: &egaku2d::glutin::event::Event<'_, ()>) {
-        if let Event::WindowEvent {event: WindowEvent::CursorMoved { device_id: _, position: p, .. }, window_id: _} = ev {
-            let total_thrust = 2.0 * self.max_thrust * (1.0 - (p.y as f32)/HEI);
-
-            let moment = 1e-1 * self.max_thrust * self.bicopter_model.prop_dist() * ((p.x as f32)/WID - 1.0/2.0);
-
-            self.bicopter_model.l_thrust = total_thrust / 2.0 + moment / (2.0 * self.bicopter_model.prop_dist());
-            self.bicopter_model.r_thrust = total_thrust / 2.0 - moment / (2.0 * self.bicopter_model.prop_dist());
-        }
+        self.controller.set_force_moment(ev)
     }
 }
 
@@ -72,19 +99,18 @@ pub fn bicopter_main() {
         &ev_loop,
         vec![
             Box::new(Bicopter{
-                max_thrust: 100.0,
-                bicopter_model: bicopter_physics::BicopterModel::new(
+                plant:bicopter_plant::BicopterPlant::new(
                     [WID/2.0,HEI/2.0].into(),
                     [0.0,0.0].into(),
                     0.0,
                     0.0,
-                    100.0,
+                    1000.0,
                     1.0,
                     100.0,
                     40.0,
                     0.0,
-                    0.0
-                )})
+                    0.0),
+                controller: BicopterForceMomentController::new(200.0, 1000.0) })
         ]);
 
     ev_loop.run(move |event, _, control_flow| main_loop(event, control_flow, &mut drawer));
