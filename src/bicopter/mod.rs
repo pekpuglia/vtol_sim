e22a8932e0b12fd3a973b362c9565f24e3ffb715
pekpuglia <pepuglia@gmail.com>
2023-08-19 12:10:53 +0200

    moved bicopter mod

--- src/bicopter/mod.rs
+++ src/bicopter/mod.rs
@@ -1,6 +1,6 @@
 use egaku2d::glutin::event::{Event, WindowEvent};
-use nalgebra::{dvector};
-use ode_solvers::{Rk4};
+use nalgebra::dvector;
+use ode_solvers::Rk4;
 
 use crate::graphical_utils::*;
 const WID: f32 = 600.0;
@@ -13,3 +13,4 @@ use bicopter_dynamics::{BicopterDynamicalModel, DynamicalSystem};
 pub mod plain_bicopter;
 
 pub mod angle_controlled_bicopter;
+
