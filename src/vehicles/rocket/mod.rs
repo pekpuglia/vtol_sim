d95cbefd7a5d934d30810cfec1353f461a0c3a5d
pekpuglia <pepuglia@gmail.com>
2024-03-08 22:47:45 +0100

    thrust arrow added

--- src/vehicles/rocket/mod.rs
+++ src/vehicles/rocket/mod.rs
@@ -1,5 +1,6 @@
 use control_systems::DynamicalSystem;
-use nalgebra::{DVector, Matrix2, Vector2};
+use egaku2d::glutin::{dpi::PhysicalPosition, event::{Event, WindowEvent}};
+use nalgebra::{dvector, DVector, Matrix2, Vector2};
 
 use crate::{geometry::{Geometry, GeometryTypes}, graphical_utils::Component, reference_frame::ReferenceFrame};
 
@@ -71,31 +72,55 @@ impl PhysicalModel for RocketModel {
                 radius: self.body_cross_section as f32 }
         );
 
+        let engine_exit = body_base - self.body_cross_section * Vector2::new(u[1].cos(), -u[1].sin());
+
         let engine = Geometry::new(
-            [1.0, 1.0, 1.0, 1.0].into(), 
+            [0.0, 0.0, 1.0, 1.0].into(), 
             *ref_frame, 
             GeometryTypes::Line { 
-                p1: body_base - self.body_cross_section / 3.0 * Vector2::new(u[1].cos(), -u[1].sin()), 
+                p1: engine_exit, 
                 p2: body_base, 
-                thickness: (self.body_cross_section/6.0) as f32 }
+                thickness: (self.body_cross_section/2.0) as f32 }
         );
 
-        vec![body, nose, engine]
+        let thrust_arrow = Geometry::new(
+            [1.0, 0.0, 0.0, 1.0].into(), 
+            *ref_frame, 
+            GeometryTypes::Arrow { 
+                start: engine_exit, 
+                end: engine_exit - 2.0 * u[0] * Vector2::new(u[1].cos(), -u[1].sin()), 
+                thickness: (self.body_cross_section/3.0) as f32}
+            );
+
+        vec![body, nose, engine, thrust_arrow]
     }
 }
 
 #[derive(Clone, Copy)]
-struct GimbalThrustInputReceiver {}
+struct GimbalThrustInputReceiver {
+    thrust_gain: f64,
+    gimbal_gain: f64
+}
 
 impl InputReceiver for GimbalThrustInputReceiver {
     fn u(&self, ev: &egaku2d::glutin::event::Event<'_, ()>) -> Option<nalgebra::DVector<f64>> {
-        todo!()
+        if let Event::WindowEvent { event: WindowEvent::CursorMoved { position: PhysicalPosition { x, y }, ..}, .. } = ev {
+
+            let thrust = (1.0 - y / HEI as f64) * self.thrust_gain;
+
+            let gimbal = (x / WID as f64 - 0.5) * self.gimbal_gain;
+
+            Some(dvector![thrust, gimbal])
+        } else {
+            None
+        }
     }
 }
 
 type PlainRocket = GenericVehicle<RocketModel, GimbalThrustInputReceiver>;
 
 impl Component for PlainRocket {
+    //abstract!!!
     fn draw(&mut self, canvas: &mut egaku2d::SimpleCanvas, dt: f32, paused: bool) {
         if !paused {
             self.update(dt as f64);
@@ -110,22 +135,25 @@ impl Component for PlainRocket {
             .map(|geom| geom.draw(canvas))
             .last();
     }
-
+    //easy to abstract
     fn receive_event(&mut self, ev: &egaku2d::glutin::event::Event<'_, ()>) {
-        ()
+        match self.input.u(ev) {
+            Some(u) => {self.u = u},
+            None => (),
+        }
     }
 }
 
 pub fn main() {
     vehicle_main(PlainRocket {
-        input: GimbalThrustInputReceiver{},
+        input: GimbalThrustInputReceiver{thrust_gain: 30.0, gimbal_gain: std::f64::consts::PI / 18.0},
         model: RocketModel { 
             dry_mass: 10.0, 
             dry_com: 0.3,
             initial_propellant_mass: 5.5, 
             exhaust_velocity: 1500.0, 
             body_length: 60.0, 
-            body_cross_section: 40.0 },
+            body_cross_section: 10.0 },
         ref_frame: ReferenceFrame::new_from_screen_frame(
             &Vector2::x(), 
             &-Vector2::y(), 
